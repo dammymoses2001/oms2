f3522e355eb0c09a014706b1ca07aa82b36fe46e
Chizaram-Igolo <chizaram@yahoo.com>
2022-12-29 19:42:44 +0100

    add product status on product table and payment status on sales report table

--- src/components/modules/visitationMapComp.js
+++ src/components/modules/visitationMapComp.js
@@ -1,6 +1,9 @@
+import React from "react";
+
 import { ModalComp } from "../ModalComp";
 
 import GoogleMapReact from "google-map-react";
+import { Link } from "react-router-dom";
 
 const Marker = ({ text }) => (
     <div>
@@ -8,9 +11,10 @@ const Marker = ({ text }) => (
     </div>
 );
 
-export default function SimpleMap() {
+export function SimpleMap() {
     const defaultProps = {
-        center: { lat: 11.99835602, lng: 10.01502627 }
+        center: { lat: 11.99835602, lng: 10.01502627 },
+        zoom: 11
     };
 
     return (
@@ -21,23 +25,78 @@ export default function SimpleMap() {
                 defaultCenter={defaultProps.center}
                 defaultZoom={defaultProps.zoom}
             >
-                <Marker text="<<Here>>" />
+                <Marker text="<<Here>>" lng={10.123485} lat={11.852342} />
             </GoogleMapReact>
         </div>
     );
 }
 
+const markerStyle = {
+    position: "absolute",
+    top: "100%",
+    left: "50%",
+    transform: "translate(-50%, -100%)"
+};
+
+class SimpleMap1 extends React.Component {
+    static defaultProps = {
+        center: {
+            lat: 6.5244,
+            lng: 3.3792
+        },
+        zoom: 11
+    };
+
+    render() {
+        return (
+            // Important! Always set the container height explicitly
+            <div style={{ height: "50vh", width: "100%" }}>
+                <GoogleMapReact
+                    bootstrapURLKeys={{
+                        key: "AIzaSyA16d9FJFh__vK04jU1P64vnEpPc3jenec"
+                    }}
+                    defaultCenter={this.props.center}
+                    defaultZoom={this.props.zoom}
+                >
+                    {this.props.locations.map((item) => {
+                        if (item.address.length !== 0) {
+                            return item.address.map((i) => {
+                                return (
+                                    <Link
+                                        to={"/" + item.name}
+                                        key={i.id}
+                                        lat={i.lat}
+                                        lng={i.lng}
+                                    >
+                                        <img
+                                            style={markerStyle}
+                                            src={"/pin.png"}
+                                            alt="pin"
+                                        />
+                                    </Link>
+                                );
+                            });
+                        }
+                    })}
+                </GoogleMapReact>
+            </div>
+        );
+    }
+}
+
+export default SimpleMap1;
+
 export const VisitationMapModal = ({ show, visitCoords, setShow }) => {
     return (
         <ModalComp
             size={"xl"}
-            title={<h5>Order List</h5>}
+            title={<h5>Visitation Map</h5>}
             show={show}
             handleClose={setShow}
             bodyText={
                 <>
-                    {visitCoords ? (
-                        <SimpleMap />
+                    {true ? (
+                        <SimpleMap1 />
                     ) : (
                         <div>
                             <div className="p-4 text-center">
